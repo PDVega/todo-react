402b378d262630a3045dd285b06bd99a4e008fe9
pdvega <pdvegaa@gmail.com>
2017-09-19 13:21:44 +0700

    update react todo list

--- src/FormTodo.js
+++ src/FormTodo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default class FormTodo extends React.Component{
-  constructor(){}
+
 
   render(){
     return(
@@ -12,4 +12,3 @@ export default class FormTodo extends React.Component{
     )
   }
 }
-
